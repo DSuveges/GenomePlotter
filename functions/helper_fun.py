df27f146897a3dcb293d52f444d9e4faedce7d46
Daniel Suveges <suvi.dani@gmail.com>
2018-12-03 22:19:08 +0000

    Stage 1 completed: generating dummy chromosome, save raw data to pkl for further processing.

--- functions/helper_fun.py
+++ functions/helper_fun.py
@@ -19,3 +19,22 @@ def generate_xy(df, min_pos, chunk_size, width, position_column = 'start', x = '
 
 def get_now():
     return strftime("%H:%M:%S", gmtime())
+
+
+class generate_xy(object):
+    '''
+    Generating x,y coordinates from genomic position and
+    the provided plot width or chunk size.
+
+    pos - genomic position
+    pixel - the size of the point taken up by one chunk.
+    width - number of chunks in one row
+    chunk_size - the number of basepairs pooled together in one chunk.
+    '''
+
+    def __init__(windowSize, width, height, pixel, zeroPos = 0, x_offset = 0, y_offset = 0):
+        self.width = width
+        self.height = height
+        self.pixel = pixel
+
+
